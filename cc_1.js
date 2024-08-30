d858b3c489e13b548c073f5d8bb729d4f16ca6a0
John Doe <johndoe@email.com>
2024-08-30 15:12:09 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task 1: Variables and Data Types
+
+let employeeName = "Jimi Hendrix";
+const employeeID = 1942;
+var isActive = true;
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
