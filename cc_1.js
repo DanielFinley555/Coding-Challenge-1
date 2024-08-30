122f129883f5cc700c08b128080ee9282f5b3cba
John Doe <johndoe@email.com>
2024-08-30 15:28:14 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -7,3 +7,16 @@ var isActive = true;
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
+
+
+
+// Task 2: Compound Data Types
+
+let products = ["Guitars", "Drums", "Pianos"]
+const productDetails = {
+    name: "Music Store"
+    price: 500.00
+    inStock: 123
+}
+
+console.log(products, productDetails);
